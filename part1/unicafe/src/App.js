4d51f29a0e41c9e80b2287d065f83be174e188b9
Burak Durak <bhdurak@gmail.com>
2022-02-01 22:30:53 +0300

    Exercise 1.9

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,6 +5,22 @@ const Heading = ({text}) => <h1>{text}</h1>
 const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 
 const Statistic = ({name, value}) => <p>{name} {value}</p>
+
+const Statistics = ({feedbackGiven, good, neutral, bad}) => {
+  if(!feedbackGiven){
+    return <p>No feedback given</p>
+  }
+  return (
+    <>
+      <Statistic name="good" value={good}/>
+      <Statistic name="neutral" value={neutral}/>
+      <Statistic name="bad" value={bad}/>
+      <Statistic name="total" value={good + bad + neutral}/>
+      <Statistic name="average" value={(good - bad)/(good + bad + neutral)}/>
+      <Statistic name="positive" value={"%" + (good*100)/(good + bad + neutral)}/>
+    </>
+  )
+}
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -18,12 +34,12 @@ const App = () => {
       <Button onClick={() => setNeutral(neutral + 1)} text="neutral"></Button>
       <Button onClick={() => setBad(bad + 1)} text="bad"></Button>
       <Heading text="statistics"/> 
-      <Statistic name="good" value={good}/>
-      <Statistic name="neutral" value={neutral}/>
-      <Statistic name="bad" value={bad}/>
-      <Statistic name="total" value={good + bad + neutral}/>
-      <Statistic name="average" value={(good - bad)/(good + bad + neutral)}/>
-      <Statistic name="positive" value={"%" + (good*100)/(good + bad + neutral)}/>
+      <Statistics 
+        feedbackGiven={good + neutral + bad > 0} 
+        good = {good}
+        neutral = {neutral}
+        bad = {bad}
+      />
     </div>
   )
 }
