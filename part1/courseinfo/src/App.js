04aa363cee8690b243696ae1f43a9daaeaf9a3cf
Burak Durak <bhdurak@gmail.com>
2022-02-01 19:45:09 +0300

    Exercise 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -28,27 +28,30 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
 
   return (
     <div>
-      <Header course={course} />
-      <Content data = {parts} />
-      <Total total={parts.map(p => p.exercises).reduce((prev, next) => prev + next)}/>
+      <Header course={course.name} />
+      <Content data = {course.parts} />
+      <Total total={course.parts.map(p => p.exercises).reduce((prev, next) => prev + next)}/>
     </div>
   )
 }
